a0c47034d1480aee9d4a2349cff7f6dc152881c8
Chinmay <chinmayshringi4@gmail.com>
2019-05-28 12:10:52 +0530

    uploading complete

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
-
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: function(req,file,cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage
+}).single('myimage');
 
 const port = 4200;
 
@@ -13,6 +23,19 @@ app.use(express.static('./public'));
 
 app.get('/', (req,res) => {
     res.render('index');
+});
+
+app.post('/upload', (req,res) => {
+    upload(req, res, (err) => {
+        if(err){
+            res.render('index',{
+                msg: err
+            });            
+        } else{
+            console.log(req.file);
+            res.send('test');
+        }
+    });
 });
 
 app.listen(port, () =>{
